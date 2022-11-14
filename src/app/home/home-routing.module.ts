c58a714aa442862fe58036cad752b4f684ebbe39
Matheus Nunes <matheusnunes.sobrinho18@gmail.com>
2022-11-14 15:09:15 -0300

    delete home and create navigation components

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,8 +1,8 @@
+import { NavigationComponent } from './navigation/navigation.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [{ path: '', component: NavigationComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    NavigationComponent
   ],
   imports: [
     CommonModule,
