586156c88f96b9470c66802d19cf80866323a463
Matheus Nunes <matheusnunes.sobrinho18@gmail.com>
2022-11-19 18:03:50 -0300

    create the job component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,13 +6,15 @@ import { HomeRoutingModule } from './home-routing.module';
 import { NavigationComponent } from './navigation/navigation.component';
 import { SearchComponent } from './search/search.component';
 import { JobSignedComponent } from './job-signed/job-signed.component';
+import { JobComponent } from './job/job.component';
 
 
 @NgModule({
   declarations: [
     NavigationComponent,
     SearchComponent,
-    JobSignedComponent
+    JobSignedComponent,
+    JobComponent
   ],
   imports: [
     CommonModule,
