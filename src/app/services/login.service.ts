676b0ec314621dfe485ce7d6f632d7dd1863b1f0
Matheus Nunes <matheusnunes.sobrinho18@gmail.com>
2022-12-05 19:13:31 -0300

    implementing login methods to success and errors

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,8 +1,9 @@
-import { Person } from './../models/person';
 import { HttpClient } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { Credentials } from '../models/credentials';
+import { Person } from './../models/person';
 
 @Injectable({
   providedIn: 'root'
@@ -16,30 +17,16 @@ export class LoginService {
 
   constructor(
     private httpClient: HttpClient,
-    private router: Router
+    private router: Router,
+    private _snackBar: MatSnackBar
   ) { }
 
   userLogin(credentials: Credentials){
-    console.log(credentials)
-    this.httpClient.post<Person>(this.APIurl, credentials, { observe: 'response' }).subscribe(data => {
-
-      if (data.status == 200) {
-        this.userAuth = true;
-        this.showToolBarEmitter.emit(true);
-        this.router.navigate(['/home', data.body?.id]);
-      } else {
-        console.log('deu errado')
-      }
-
-    })
-
-    /*this.httpClient.get<Credentials>(this.APIurl, { observe: 'response' }).subscribe(data => {
-      console.log(data.status)
-      this.userAuth = true;
-      this.showToolBarEmitter.emit(true);
-      this.router.navigate(['/home', data.body?.id]);
-    })*/
-
+    this.httpClient.post<Person>(this.APIurl, credentials, { observe: 'response' })
+      .subscribe({
+        next: (n) => {this.loginSuccess(n.body?.id)},
+        error: (r) => {this.loginError(r.error)}
+      })
   }
 
   logout(){
@@ -48,5 +35,15 @@ export class LoginService {
 
   getUserAuth(){
     return this.userAuth;
+  }
+
+  loginError(message: string){
+    this._snackBar.open(message, 'OK', { duration: 5000 })
+  }
+
+  loginSuccess(id: any){
+    this.userAuth = true;
+    this.showToolBarEmitter.emit(true);
+    this.router.navigate(['/home', id]);
   }
 }
